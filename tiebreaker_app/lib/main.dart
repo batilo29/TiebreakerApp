d003593f87eba97e1d0afa4bd8c8a37f09d94b03
jasperbatilo <jasperbatilo52@gmail.com>
2026-03-31 13:40:44 +0800

    smart decision-making assistant app

---
+++ tiebreaker_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/decision_service.dart';
+import 'screens/home_screen.dart';
+
+void main() => runApp(const TieBreakerApp());
+
+class TieBreakerApp extends StatelessWidget {
+  const TieBreakerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DecisionService())
+      ],
+
+      child: MaterialApp(
+        title: 'TieBreakerApp',
+        theme: ThemeData(colorSchemeSeed: Colors.deepPurple),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
+
